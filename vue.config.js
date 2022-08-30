955d433c4ff9b8adbc4dfd9e04676740aa5771c8
tian <1192556828@qq.com>
2022-08-30 14:18:01 +0800

    项目初始化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave:false,
+  configureWebpack:{
+    devServer:{
+      port: 3000,
+      open: true,
+      host: 'localhost',
+    }
+  }
 })
